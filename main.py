2e9d89235f2772756b674e8f6fdea764cdc71946
Erick Duarte <erickod@gmail.com>
2023-09-10 21:17:36 -0300

    chore: add main.py to .gitignore

--- main.py
+++
@@ -1,9 +0,0 @@
-from uuid import uuid1
-
-from credits_account.domain.entities.credit_account import CreditAccount
-
-sut = CreditAccount(company_id=uuid1(), credit_state_list=[])
-sut.add(5, "asdf", "subscription")
-sut.add(5, "asdfasfd", "subscription")
-sut.consume(6, "asdfasdf")
-print(sut)
